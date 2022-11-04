84de8ba2d065932518de7589347def6ff621745a
隔壁班的白芷 <1069936623@qq.com>
2022-11-04 14:26:38 +0800

    express.json和express.urlencoded的使用

--- .history/express.json中间件的使用_20221104142231.js
+++ .history/express.json中间件的使用_20221104142231.js
@@ -3,7 +3,7 @@ const app = express()
     //使用express.json()解析
 app.use(express.json())
     //使用express.urlencoded解析
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded())
 
 app.post('/user', (req, res) => {
     console.log(req.body)
@@ -11,7 +11,6 @@ app.post('/user', (req, res) => {
 
 app.post('/book', (req, res) => {
     console.log(req.body)
-    res.send('ok')
 })
 app.listen(80, () => {
     console.log('run')
--- .history/express.json中间件的使用_20221104142407.js
+++ .history/express.json中间件的使用_20221104142407.js
@@ -3,7 +3,7 @@ const app = express()
     //使用express.json()解析
 app.use(express.json())
     //使用express.urlencoded解析
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded())
 
 app.post('/user', (req, res) => {
     console.log(req.body)
